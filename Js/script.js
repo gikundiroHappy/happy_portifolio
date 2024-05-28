43e4f0a27eb901b80a8cbf8668bc11169e25defb
gikundiroHappy <gikundirohappy@gmail.com>
2024-05-28 22:33:13 +0200

    added navbar

--- Js/script.js
+++ Js/script.js
@@ -10,3 +10,23 @@ var swiperPortfolio = new Swiper('.mySwiper', {
       clickable: true,
     }
   });
+
+  const shownav = document.getElementById("show")
+  const nav = document.getElementById("nav")
+  const closeNavButton = document.getElementById("close");
+  const bottomBar = document.getElementById("bottom-bar");
+  const navLink = document.getElementsByClassName("navlink")
+
+  shownav.addEventListener("click",Handleshow)
+  closeNavButton.addEventListener("click", handleCloseNav);
+  navLink.addEventListener("click", handleCloseNav);
+
+  function Handleshow(){
+    nav.classList.remove("hidden");
+    bottomBar.classList.add("hidden");
+  }
+
+  function handleCloseNav() {
+    nav.classList.add("hidden");
+    bottomBar.classList.remove("hidden");
+  }
